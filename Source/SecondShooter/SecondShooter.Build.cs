aea866fadebc2be2dd7ee8c31f28462202576faf
JJBRosenberg <85509521+JJBRosenberg@users.noreply.github.com>
2023-01-06 20:40:51 +0100

    Setting up gameplaysystm

--- Source/SecondShooter/SecondShooter.Build.cs
+++ Source/SecondShooter/SecondShooter.Build.cs
@@ -8,6 +8,8 @@ public class SecondShooter : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "GameplayTasks", "UMG" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "GameplayTasks", "GameplayTags", "UMG" });
+
+		PrivateDependencyModuleNames.AddRange(new string[] { "GameplayAbilities" , "GameplayAbilitiesEditor" , "GameplayTags" , "GameplayTagsEditor", "GameplayTasks", "Slate" , "SlateCore" , "UMG" });
 	}
 }
